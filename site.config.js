8db2eb93543136326e669ec010ffafcd8577dd24
Magnet Pro <macbookpro@Magnet-MacBook-Pro.local>
2018-11-05 13:07:57 +0200

    first commit

---
+++ site.config.js
@@ -0,0 +1,8 @@
+const projects = require('./src/data/projects')
+module.exports = {
+  site: {
+    title: 'NanoGen',
+    description: 'Micro Static Site Generator in Node.js',
+    projects
+  }
+}
